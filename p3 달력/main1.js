891e1d6e1ad37f36b50c13b4a2b41ee2c074d314
liondot <51257576+liondot@users.noreply.github.com>
2022-07-29 14:56:13 +0900

    달력의 년과 날짜를 동적으로 출력하는 과정

--- p3 달력/main1.js
+++ p3 달력/main1.js
@@ -25,3 +25,47 @@ for(let i = day; i < day + d_length; i++){
 // caption 연도, 월 표시 
 caption_year.innerHTML = year;
 caption_month.innerHTML = month + 1;
+
+// 특정 시간때가(2022년) 아닌사용자의 요청에 따라 월별 달력 출력 하는 함수
+function calender(new_year, new_month) {
+  // 특정 연월을 시작일부터 조회(year, month, date )
+  let d = new Date(new_year, new_month-1, 1),
+    // 월별 일수 구하기 
+    d_length = 32 - new Date(new_year, new_month-1, 32).getDate(),
+    year = d.getFullYear(),
+    month = d.getMonth(),
+    date = d.getDate(),
+    day = d.getDay(),
+
+    // caption 영역 날짜 표시 객체 
+    caption_year = document.querySelector('.year');
+        caption_month = document.querySelector('.month');
+    let start_day = document.querySelectorAll('tr td');
+
+    // 테이블 초기화 
+    for(let i = 0; i<start_day.length; i++){
+      start_day[i].innerHTML = '$nbsp';
+    }
+
+
+  }
+
+  
+  (function() {
+    let prev = document.getElementById('prev');
+    next=document.getElementById('next');
+    year=new Date().getFullYear();
+    month = new Date().getMonth() + 1;
+
+    calender(year, month);
+
+    // 이전 달 , 다음 달 버튼 이벤트 핸들러 
+    prev.onclick = function(){
+      calender(year, --month);
+    };
+    next.onclick = function(){
+      calender(year, ++month)
+    }
+
+  })();
+
